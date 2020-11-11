95fb3d106b1a482e0f2a3be8f0c6c82ded4aee3c
Nathan <nath.king93@gmail.com>
2020-11-11 17:51:03 +1100

    Change responsive design

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -5,21 +5,21 @@ import Subheading from "../Subheading/Subheading";
 export default function About() {
   return (
     <div className={styles.container} id="about">
+      <div className={styles.photo}>
+        {/* <img className={styles.img} src="/me.jpg" /> */}
+      </div>
       <div className={styles.text}>
         <Subheading number="i."> About Me</Subheading>
         <p>
-          I have always been passionate about technology. Having been born in
-          the early 90s, I was able to watch the birth and evolution of the
-          digital world. It was as a teenager that I first began to play around
-          with and build my own websites. I have always been intrigued by the
+          I have always been passionateabout technology. Having been born in the
+          early 90s, I was able to watch the birth and evolution of the digital
+          world. It was as a teenager that I first began to play around with and
+          build my own websites. I have always been intrigued by the
           relationship between human beings and the digital world that has been
           imposed around us. I began studying psychology at university and to
           explore this relationship. My passions, however, have always lied with
           coding and the creative aspect of web development.
         </p>
-      </div>
-      <div className={styles.photo}>
-        {/* <img className={styles.img} src="/me.jpg" /> */}
       </div>
     </div>
   );
--- components/Interests/Interests.tsx
+++ components/Interests/Interests.tsx
@@ -3,6 +3,7 @@ import styles from "./Interests.module.scss";
 import Skill from "../Skill/Skill";
 import Subheading from "../Subheading/Subheading";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import {
   faDesktop,
   faFileCode,
@@ -25,63 +26,38 @@ export default function Interests() {
   return (
     <div className={styles.container} id="skills">
       <Subheading number="ii."> Skills</Subheading>
-      {!on ? (
-        <div className={styles.skills}>
-          <Skill>
-            Web Dev{" "}
-            <FontAwesomeIcon icon={faLaptopCode} className={styles.icon} />
-          </Skill>
-          <Skill>
-            Web Design{" "}
-            <FontAwesomeIcon icon={faDesktop} className={styles.icon} />
-          </Skill>
-          <Skill>
-            React <FontAwesomeIcon icon={faReact} className={styles.icon} />
-          </Skill>
-          <Skill full={true} onClick={toggle}>
-            See More...
-          </Skill>
-        </div>
-      ) : (
-        <>
-          <div className={styles.skills}>
-            <Skill>
-              Web Dev{" "}
-              <FontAwesomeIcon icon={faLaptopCode} className={styles.icon} />
-            </Skill>
-            <Skill>
-              Web Design{" "}
-              <FontAwesomeIcon icon={faDesktop} className={styles.icon} />
-            </Skill>
-            <Skill>
-              React <FontAwesomeIcon icon={faReact} className={styles.icon} />
-            </Skill>
-            <Skill>
-              CSS-in-JS{" "}
-              <FontAwesomeIcon icon={faFileCode} className={styles.icon} />
-            </Skill>
-            <Skill>
-              Sass/SCSS{" "}
-              <FontAwesomeIcon icon={faSass} className={styles.icon} />
-            </Skill>
-            <Skill>
-              JS (ES6+){" "}
-              <FontAwesomeIcon icon={faJsSquare} className={styles.icon} />
-            </Skill>
-            <Skill>
-              Wordpress
-              <FontAwesomeIcon
-                icon={faWordpressSimple}
-                className={styles.icon}
-              />
-            </Skill>
-            <Skill>
-              Node.js
-              <FontAwesomeIcon icon={faNodeJs} className={styles.icon} />
-            </Skill>
-          </div>
-        </>
-      )}
+      <div className={styles.skills}>
+        <Skill>
+          Web Dev{" "}
+          <FontAwesomeIcon icon={faLaptopCode} className={styles.icon} />
+        </Skill>
+        <Skill>
+          React <FontAwesomeIcon icon={faReact} className={styles.icon} />
+        </Skill>
+        <Skill>
+          Design
+          <FontAwesomeIcon icon={faDesktop} className={styles.icon} />
+        </Skill>
+        <Skill>
+          CSS-in-JS{" "}
+          <FontAwesomeIcon icon={faFileCode} className={styles.icon} />
+        </Skill>
+        <Skill>
+          Sass/SCSS <FontAwesomeIcon icon={faSass} className={styles.icon} />
+        </Skill>
+        <Skill>
+          JS (ES6+){" "}
+          <FontAwesomeIcon icon={faJsSquare} className={styles.icon} />
+        </Skill>
+        <Skill>
+          Wordpress
+          <FontAwesomeIcon icon={faWordpressSimple} className={styles.icon} />
+        </Skill>
+        <Skill>
+          Node.js
+          <FontAwesomeIcon icon={faNodeJs} className={styles.icon} />
+        </Skill>
+      </div>
     </div>
   );
 }
