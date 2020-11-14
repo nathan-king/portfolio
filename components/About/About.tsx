8be30cc49dd07f46a0d42330977191f688be8004
Nathan <nath.king93@gmail.com>
2020-11-14 20:54:45 +1100

    Review content

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -11,14 +11,13 @@ export default function About() {
       <div className={styles.text}>
         <Subheading number="i."> About Me</Subheading>
         <p>
-          I have always been passionateabout technology. Having been born in the
+          I have always been passionate about technology. Being been born in the
           early 90s, I was able to watch the birth and evolution of the digital
-          world. It was as a teenager that I first began to play around with and
-          build my own websites. I have always been intrigued by the
-          relationship between human beings and the digital world that has been
-          imposed around us. I began studying psychology at university and to
-          explore this relationship. My passions, however, have always lied with
-          coding and the creative aspect of web development.
+          world. It was as a teenager that I first began to play around with
+          code and build my own websites. I have always been intrigued by the
+          relationship between human beings and the digital world. I began
+          studying psychology at university and to explore this relationship.
+          Programming and web development have always been my greatest passion.
         </p>
       </div>
     </div>
