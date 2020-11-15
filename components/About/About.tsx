a2414950048ba6c7e2b71cd9821bdb1ca92004af
Nathan <nath.king93@gmail.com>
2020-11-15 15:04:23 +1100

    Adjust responsiveness for extra large screens

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -11,12 +11,12 @@ export default function About() {
       <div className={styles.text}>
         <Subheading number="i."> About Me</Subheading>
         <p>
-          I have always been passionate about technology. Being been born in the
+          I have always been passionate about technology. Being born in the
           early 90s, I was able to watch the birth and evolution of the digital
           world. It was as a teenager that I first began to play around with
           code and build my own websites. I have always been intrigued by the
           relationship between human beings and the digital world. I began
-          studying psychology at university to explore this relationship.
+          studying psychology at university and to explore this relationship.
           Programming and web development have always been my greatest passion.
         </p>
       </div>
